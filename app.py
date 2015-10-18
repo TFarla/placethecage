9bad7fc28557a42cac9c91209c19599d91378fd5
Thomas Farla <thomas-farla@hotmail.com>
2015-10-18 13:43:26 +0200

    Make app run in dev mode

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_path='/public')
 
 @app.route('/')
 def index():
     return 'hello world!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
