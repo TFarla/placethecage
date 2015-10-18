29a4250ca5765556802fca6c85a1c0bb58bae7de
Thomas Farla <thomas-farla@hotmail.com>
2015-10-18 14:14:53 +0200

    Show nic pic

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, send_file
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__, static_path='/public')
 
@@ -11,7 +13,15 @@ def index():
 def get_nic_pic(width, height):
     if height is None:
         height = width
-    return ''
+
+    img = Image.open('public/images/nic1.jpg')
+    out = img.resize((width, height))
+
+    img_io = BytesIO()
+    out.save(img_io, 'JPEG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True)
