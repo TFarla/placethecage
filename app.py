dd803a6a40923dafa91acde5b39b532547fdb25c
Thomas Farla <thomas-farla@hotmail.com>
2015-10-18 15:25:44 +0200

    Show best fitting image

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_file
 from PIL import Image
 from io import BytesIO
+from os import listdir
+from pathlib import Path
 
-app = Flask(__name__, static_path='/public')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -14,7 +16,7 @@ def get_nic_pic(width, height):
     if height is None:
         height = width
 
-    img = Image.open('public/images/nic1.jpg')
+    img = Image.open(get_best_suited_pic(width, height))
     out = img.resize((width, height))
 
     img_io = BytesIO()
@@ -22,6 +24,13 @@ def get_nic_pic(width, height):
     img_io.seek(0)
 
     return send_file(img_io, mimetype='image/jpeg')
+
+def get_best_suited_pic(width, height):
+    aspect_ratio = width / height
+    images_path = Path('private/images')
+    ratios = [0.7, 1.0, 1.2, 1.3, 1.5, 1.8]
+    ratio = min(ratios, key=lambda x:abs(x - aspect_ratio))
+    return str(images_path / 'nic{}.jpg'.format(ratio))
 
 if __name__ == '__main__':
     app.run(debug=True)
