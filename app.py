832a11fd217b2ecbd82b75cf9375ecd1d4ec9908
Thomas Farla <thomas-farla@hotmail.com>
2015-10-18 13:59:30 +0200

    Handle route

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__, static_path='/public')
 def index():
     return 'hello world!'
 
+@app.route('/<int:width>', defaults={'height': None})
+@app.route('/<int:width>/<int:height>')
+def get_nic_pic(width, height):
+    if height is None:
+        height = width
+    return ''
+
 if __name__ == '__main__':
     app.run(debug=True)
